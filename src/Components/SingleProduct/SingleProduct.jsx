be1c99e6fed85ccca31a42bffe553c1ffe8223bd
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-29 06:01:55 +0600

    added product card with dynamic data

--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const SingleProduct = (props) => {
-    const { image, category, title, price, description} =props.product;
+    const { image, category, title, price, rating} = props.product;
     console.log(props.product)
     return (
-        <div>
-            
+        <div className="card  bg-pink-300 shadow-xl">
+            <figure><img className='w-64 h-64 m-4 rounded-lg' src={image} alt="Shoes" /></figure>
+            <div className="card-body text-black">
+                <h2 className="card-title">{category}</h2>
+                <p>Price: ${price}</p>
+                <p>Title: {title}</p>
+                <p>Rating: {rating.rate} Stars,  Count: {rating.count}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary block">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/products/Products.jsx
+++ src/Components/products/Products.jsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import SingleProduct from '../SingleProduct/SingleProduct';
+import './Products.css';
 
 
 const Products = () => {
     const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://fakestoreapi.com/products`)
-        .then(res => res.json())
-        .then(data => setProducts(data));
+            .then(res => res.json())
+            .then(data => setProducts(data));
     }, [])
     return (
-        <div>
-            {
-                products.map(product =><SingleProduct product ={product}></SingleProduct>)
-            }
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <SingleProduct product={product}></SingleProduct>)
+                }
+            </div>
+            <div className='cart-container'>
+                <h1>Order Summary</h1>
+            </div>
         </div>
     );
 };
